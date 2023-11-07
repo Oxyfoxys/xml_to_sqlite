41fd9068dd44cf2914c52a80f1983d766b41f33f
oxyfoxys <oxyfoxys@yandex.ru>
2023-11-07 16:09:35 +0300

    Обновила тест и парсер xml

--- test/test_xml.py
+++ test/test_xml.py
@@ -5,11 +5,11 @@ import xml_parser
 
 
 class Test(TestCase):
-    def test_download_xml_plant(self):
-        app.download_file('https://www.w3schools.com/xml/plant_catalog.xml', 'resources',
-                          'plant_catalog_xml' + app.get_date_and_time() + '.xml')
+    def test_download_xml_cd(self):
+        app.download_file('https://www.w3schools.com/xml/cd_catalog.xml', 'resources',
+                          'cd_catalog_xml' + app.get_date_and_time() + '.xml')
 
-    def test_parse_xml_plant(self):
-        data = xml_parser.get_xml_plant('https://www.w3schools.com/xml/plant_catalog.xml')
+    def test_parse_xml_cd(self):
+        data = xml_parser.get_xml_plant('https://www.w3schools.com/xml/cd_catalog.xml')
         for i in data:
             print(i)
--- xml_parser.py
+++ xml_parser.py
@@ -3,7 +3,7 @@ import urllib.request as urllib2
 import xmltodict
 
 
-def get_xml_plant(url):
+def get_xml_cd(url):
     file = urllib2.urlopen(url)
     data = file.read()
     file.close()
@@ -13,8 +13,8 @@ def get_xml_plant(url):
     # print('breakfast_menu')
     # print(str(len(products['food'])))
     my_array = []
-    for position in products['PLANT']:
-        my_list = [position['COMMON'], position['BOTANICAL'], position['ZONE'], position['LIGHT'], position['PRICE'], position['AVAILABILITY']]
+    for position in products['CD']:
+        my_list = [position['TITLE'], position['ARTIST'], position['COUNTRY'], position['COMPANY'], position['PRICE'], position['YEAR']]
         my_array.append(my_list)
 
         # print('')
